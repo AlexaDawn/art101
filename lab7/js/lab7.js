0dd28f09c6e45463dc9b1fab03e9ea44573e3c4e
AlexaDawn <91585115+AlexaDawn@users.noreply.github.com>
2021-10-25 22:17:54 -0700

    lab changes

---
+++ lab7/js/lab7.js
@@ -0,0 +1,24 @@
+/*
+Author: Alexa Marquez
+Created: 10.25.2021
+
+*/
+
+function sortUserName() {
+  var userName = window.prompt("hewwo! pwease tell me your name so I may sort it!");
+  console.log("userName =", userName);
+  // split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  // sort the nameArray
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  // join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+//Output
+document.writeln("owo, I've fixed your name:",
+  sortUserName(), "</br>");
